6c1dab90430621e7af40cf72ce28eedf2be05fde
pnxu <fcaavv@gmail.com>
2023-10-27 00:18:37 -0300

    chore: update toast message in Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,90 +15,91 @@ function Contact() {
     try {
       sendForm(data);
       reset();
-      toast.success("Form submitted successfully!");
+      toast.success("Contact message submitted successfully!");
     } catch (error) {
-      console.error("Error adding document: ", error);
-      toast.error("Error submitting form");
+      toast.error("An error occurred while submitting the contact form.");
     }
   };
 
   return (
-    <div className="container text-center mt-5">
-      <h2>Contact me</h2>
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <div className="contact-icons mb-2">
-            <a
-              href="mailto:fcaavv@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa fa-envelope fa-lg mx-2 text-dark"></i>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/fcaavv"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa fa-linkedin fa-lg mx-2 text-dark"></i>
-            </a>
-            <a
-              href="https://github.com/pnxu"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa fa-github fa-lg mx-2 text-dark"></i>
-            </a>
+    <section id="contact" className="py-4">
+      <div className="container text-center mt-5">
+        <h2>Contact me</h2>
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="contact-icons mb-2">
+              <a
+                href="mailto:fcaavv@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-envelope fa-lg mx-2 text-dark"></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/fcaavv"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-linkedin fa-lg mx-2 text-dark"></i>
+              </a>
+              <a
+                href="https://github.com/pnxu"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-github fa-lg mx-2 text-dark"></i>
+              </a>
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="form-group mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Name"
+                  {...register("name", { required: true })}
+                />
+                {errors.name && (
+                  <span className="text-danger">Name is required</span>
+                )}
+              </div>
+              <div className="form-group mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email"
+                  {...register("email", {
+                    required: true,
+                    pattern: /^\S+@\S+$/i,
+                  })}
+                />
+                {errors.email && (
+                  <span className="text-danger">Valid email is required</span>
+                )}
+              </div>
+              <div className="form-group mb-3">
+                <textarea
+                  className="form-control"
+                  placeholder="Message"
+                  {...register("message", { required: true })}
+                />
+                {errors.message && (
+                  <span className="text-danger">Message is required</span>
+                )}
+              </div>
+              <button type="submit" className="btn btn-dark mb-3">
+                Submit
+              </button>
+              <ToastContainer
+                className="custom-toast"
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar
+              />
+            </form>
           </div>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="form-group mb-3">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Name"
-                {...register("name", { required: true })}
-              />
-              {errors.name && (
-                <span className="text-danger">Name is required</span>
-              )}
-            </div>
-            <div className="form-group mb-3">
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Email"
-                {...register("email", {
-                  required: true,
-                  pattern: /^\S+@\S+$/i,
-                })}
-              />
-              {errors.email && (
-                <span className="text-danger">Valid email is required</span>
-              )}
-            </div>
-            <div className="form-group mb-3">
-              <textarea
-                className="form-control"
-                placeholder="Message"
-                {...register("message", { required: true })}
-              />
-              {errors.message && (
-                <span className="text-danger">Message is required</span>
-              )}
-            </div>
-            <button type="submit" className="btn btn-dark">
-              Submit
-            </button>
-            <ToastContainer
-              className="custom-toast"
-              position="top-right"
-              autoClose={3000}
-              hideProgressBar
-            />
-          </form>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
