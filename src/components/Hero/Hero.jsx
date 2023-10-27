cbd66548a4eaf6cacd4e4a23ecb30b6e9c3e872c
pnxu <fcaavv@gmail.com>
2023-10-27 00:16:18 -0300

    feat: add icon PNGs to assets and update hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -17,11 +17,55 @@ function Hero() {
           </div>
           <div className="col-md-6 order-md-1">
             <div className="hero-content mt-2">
-              <h1>Front-End Developer</h1>
+              <h1 className="display-4 font-weight-bolder">
+                Front-End Developer
+              </h1>
+
               <p>
                 Hi, I'm Francisca Vega. A Front-End Developer based in Santiago,
                 Chile.
               </p>
+              <div className="tech-skills mt-4">
+                <div className="tech-skills mt-4">
+                  <div className="tech-skills mt-4">
+                    <img
+                      src="../../../public/assets/html-5.png"
+                      alt="HTML"
+                      className="img-responsive mx-2 img-fluid img-thumbnail"
+                    />
+                    <img
+                      src="../../../public/assets/css-3.png"
+                      alt="CSS"
+                      className="img-responsive mx-2 img-fluid img-thumbnail"
+                    />
+                    <img
+                      src="../../../public/assets/react.png"
+                      alt="React"
+                      className="img-responsive mx-2 img-fluid img-thumbnail"
+                    />
+                    <img
+                      src="../../../public/assets/js.png"
+                      alt="JavaScript"
+                      className="img-responsive mx-2 img-fluid img-thumbnail"
+                    />
+                    <img
+                      src="../../../public/assets/firebase.png"
+                      alt="Firebase"
+                      className="img-responsive mx-2 img-fluid img-thumbnail"
+                    />
+                    <img
+                      src="../../../public/assets/figma.png"
+                      alt="Figma"
+                      className="img-responsive mx-2 img-fluid img-thumbnail"
+                    />
+                    <img
+                      src="../../../public/assets/bootstrap.png"
+                      alt="Bootstrap"
+                      className="img-responsive mx-2 img-fluid img-thumbnail"
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
