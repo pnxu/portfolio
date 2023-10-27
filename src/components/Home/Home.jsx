a9805b2956e83f0639b4da8d90007a9b63440473
pnxu <fcaavv@gmail.com>
2023-10-26 21:21:34 -0300

    feat: add Projects component to Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Hero from "../Hero/Hero";
+import Projects from "../Projects/Projects";
 
 function Home() {
   return (
     <div className="home-container">
       <Navbar />
       <Hero />
+      <Projects />
     </div>
   );
 }
