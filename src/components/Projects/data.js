ef17bc5f01144134c63cc5307950e6f06258e7b7
pnxu <fcaavv@gmail.com>
2023-10-26 21:08:20 -0300

    feat: add project data & project images to public/assets

---
+++ src/components/Projects/data.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+export const projects = [
+  {
+    title: "Portfolio",
+    description:
+      "My personal portfolio featuring my projects, skills, and contact information.",
+    technologies:
+      "ReactJS, React Boostrap, React Hook Form, React Toastify, Firestore, Figma",
+    imageSrc: "/public/assets/mockup_portfolio.jpg",
+    repoLink: "",
+    websiteLink: "",
+  },
+  {
+    title: "Burger Queen",
+    description:
+      "Web application using React.js to efficiently manage restaurant order taking, including user authentication and role management functionality.",
+    technologies: "ReactJS, HTTP Request, Axios, Postman, Figma",
+    imageSrc: "/public/assets/mockup_burgerqueen.jpeg",
+    repoLink: "https://github.com/pnxu/DEV003-burger-queen-api-client",
+    websiteLink: "",
+  },
+  {
+    title: "MD-Links",
+    description:
+      "A markdown link extraction tool that enables link visualization and HTTP status validation, along with total, unique, and broken link statistics.",
+    technologies: "NodeJS, NPM, JavaScript, Axios, Colors",
+    imageSrc: "/public/assets/mockup_mdlinks.jpeg",
+    repoLink: "https://github.com/pnxu/DEV003-md-links",
+    websiteLink: "https://www.npmjs.com/package/@pnxu/md-links",
+  },
+  {
+    title: "Social Network",
+    description:
+      "Single Page Application designed for sharing book reviews and recommendations, featuring user authentication and database integration.",
+    technologies: "Javascript, Firebase, HTML, CSS, Jest, Figma",
+    imageSrc: "/public/assets/mockup_socialnetwork.jpg",
+    repoLink: "https://github.com/pnxu/DEV003-social-network",
+    websiteLink: "https://dev-003-purrfect-books-o485x9nx7-pnxu.vercel.app/#/",
+  },
+  {
+    title: "Data Lovers",
+    description:
+      "Ghiblipedia is a responsive website showcasing Studio Ghibli data, with filtering by years, directors, and more.",
+    technologies: "JavaScript, JSON, HTML, CSS, Jest, Figma",
+    imageSrc: "/public/assets/mockup_datalovers.jpeg",
+    repoLink: "https://github.com/pnxu/dataLovers-SG-MF",
+    websiteLink: "https://pnxu.github.io/dataLovers-SG-MF/",
+  },
+  {
+    title: "Card Validation",
+    description:
+      "This website validates credit card numbers using the Luhn algorithm, with unit tests for accuracy and card number masking.",
+    technologies: "JavaScript, HTML, CSS",
+    imageSrc: "/public/assets/mockup_cardvalidation.jpg",
+    repoLink: "https://github.com/pnxu/DEV003-card-validation",
+    websiteLink: "https://pnxu.github.io/DEV003-card-validation/",
+  },
+];
