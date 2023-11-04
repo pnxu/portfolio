0ad11c6429f2fc0e6b59c4a2da785994ce07a808
pnxu <fcaavv@gmail.com>
2023-11-04 17:02:57 -0300

    feat(techskills): create TechSkillsIcons component

---
+++ src/components/TechSkills/TechSkillsIcons.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+function TechSkills() {
+  return (
+    <div className="container mt-4 ms">
+      <img
+        src="../../../public/assets/html-5.png"
+        alt="HTML"
+        className="img-responsive mx-2 img-fluid img-thumbnail"
+      />
+      <img
+        src="../../../public/assets/css-3.png"
+        alt="CSS"
+        className="img-responsive mx-2 img-fluid img-thumbnail"
+      />
+      <img
+        src="../../../public/assets/react.png"
+        alt="React"
+        className="img-responsive mx-2 img-fluid img-thumbnail"
+      />
+      <img
+        src="../../../public/assets/js.png"
+        alt="JavaScript"
+        className="img-responsive mx-2 img-fluid img-thumbnail"
+      />
+      <img
+        src="../../../public/assets/firebase.png"
+        alt="Firebase"
+        className="img-responsive mx-2 img-fluid img-thumbnail"
+      />
+      <img
+        src="../../../public/assets/figma.png"
+        alt="Figma"
+        className="img-responsive mx-2 img-fluid img-thumbnail"
+      />
+      <img
+        src="../../../public/assets/bootstrap.png"
+        alt="Bootstrap"
+        className="img-responsive mx-2 img-fluid img-thumbnail"
+      />
+    </div>
+  );
+}
+
+export default TechSkills;
