203e96e3646821b1e9ee027e4fe5fbd4cf4fbd3e
pnxu <fcaavv@gmail.com>
2023-11-04 17:01:26 -0300

    chore(projects): remove whitespace

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,7 +5,7 @@ import { projects } from "./data";
 function Projects() {
   return (
     <section id="projects" className="bg-light p-4">
-      <div className="container ">
+      <div className="container">
         <h2 className="text-center mb-5">Projects</h2>
         <div className="row">
           {projects.map((project, index) => (
